0e90b40714c97dcba79cb3399f92c46aec28eef1
08_CLEARESTA DANISH RYU RAMADHAN <danish.tkjsmeksa25@gmail.com>
2026-08-31 09:55:56 +0700

    Modul Bilangan

---
+++ Bilangan.py
@@ -0,0 +1,22 @@
+def Mencari_Bilangan_Prima(x):
+    bilangan_prima = []
+
+    for angka in range(2, x + 1):
+        prima = True
+
+        for i in range(2, angka):
+            if angka % i == 0:
+                prima = False
+                break
+
+        if prima:
+            bilangan_prima.append(angka)
+
+    return bilangan_prima
+
+
+def Cek_Ganjil_Genap(n):
+    if n % 2 == 0:
+        return "Genap"
+    else:
+        return "Ganjil"
---
+++ Matematika.py
@@ -0,0 +1,15 @@
+import Bilangan
+
+print(Bilangan.__file__)
+print(hasattr(Bilangan, "Cek_Ganjil_Genap"))
+
+# Input bilangan prima
+x = int(input("Masukkan angka: "))
+
+print(f"Bilangan prima dari 1 sampai {x} adalah: {Bilangan.Mencari_Bilangan_Prima(x)}")
+
+
+# Input ganjil genap
+x = int(input("Masukkan angka: "))
+
+print(f"{x} adalah bilangan {Bilangan.Cek_Ganjil_Genap(x)}")
